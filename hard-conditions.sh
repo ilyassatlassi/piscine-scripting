9bf1c71d5f044ef66ccd8a9837594d94876c2157
ilyassatlassi <atlassiilyass@gmail.com>
2026-04-01 17:40:11 +0100

    hard conditions

--- hard-conditions.sh
+++ hard-conditions.sh
@@ -1,7 +1,7 @@
 
 if [[ -f "$1" && -x "$1" ]]
 then
-    echo "File is executable."
+    echo "File is executable"
 else
-    echo "File is not an executable or does not exist."
+    echo "File is not an executable or does not exist"
 fi
