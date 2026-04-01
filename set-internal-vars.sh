ab18977db6cedf154053a99e822df65e0720060c
ilyassatlassi <atlassiilyass@gmail.com>
2026-04-01 12:05:23 +0100

    set internal vars

---
+++ set-internal-vars.sh
@@ -0,0 +1,9 @@
+export MY_MESSAGE="Hello World"
+export MY_NUM="100"
+export MY_PI="3.142"
+export MY_ARR="one, two, three, four, five"
+
+echo "$MY_MESSAGE"
+echo "$MY_NUM"
+echo "$MY_PI"
+echo "$MY_ARR"
