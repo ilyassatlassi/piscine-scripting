6578e6e1e18e9a85d3eb8b1d30e319206a9a04a9
ilyassatlassi <atlassiilyass@gmai.com>
2026-04-05 11:19:27 +0100

    add division

--- division.sh
+++ division.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]; then
+if [[ $# -lt 2 ]]; then
 	echo "Error: two numbers must be provided"
 
 elif ! [[ $1 =~ ^-?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]+$ ]]; then
 	echo "Error: both arguments must be integers"
 
-elif [ $(echo "$2 == 0" | bc) -eq 1 ]; then
+elif [ $2 == "0" ]; then
 	echo "Error: division by zero is not allowed"
 
 else
