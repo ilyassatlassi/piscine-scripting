7fa1ee32adb4d3fd9dafadca4c706aecab7ca416
ilyassatlassi <atlassiilyass@gmai.com>
2026-04-05 12:04:17 +0100

    add array-selector

--- array-selector.sh
+++ array-selector.sh
@@ -1,8 +1,8 @@
-COLORS=('red' 'blue' 'green' 'white' 'black')
+COLORS=(red blue green white black)
 
 if [[ ! $1 =~ ^[0-9]+$ || $1 -le 0 || $1 -gt ${#COLORS[@]} ]]; then
 	echo 'Error'
 else 
-	Pos = $(($1 -1))
+	Pos=$(($1 -1))
 	echo ${COLORS[$Pos]}
 fi
