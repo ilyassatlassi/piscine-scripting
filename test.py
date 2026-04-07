568c5385adb1b32d923b884db74ec64b1ece666f
ilyassatlassi <atlassiilyass@gmai.com>
2026-04-07 12:05:58 +0100

    numerical

---
+++ test.py
@@ -0,0 +1,7 @@
+import numerical_operations
+
+print(numerical_operations.add(2, 2))
+print(numerical_operations.subtract(10, 5))
+print(numerical_operations.multiply(3, 4))
+print(numerical_operations.power(3, 3))
+print(numerical_operations.square_root(3))
