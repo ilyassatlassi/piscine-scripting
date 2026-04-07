15831242bd69a985782d679dc0d6ca569780fb2b
ilyassatlassi <atlassiilyass@gmai.com>
2026-04-07 13:54:06 +0100

    clean_the_list

--- clean_the_list.py
+++ clean_the_list.py
@@ -2,14 +2,13 @@ def clean_list(shopping_list):
     if not shopping_list:
         return []
 
-    cleaned = shopping_list.copy()
 
-    if 'milk' not in cleaned:
-        cleaned.append('milk')
+    if 'milk' not in shopping_list:
+        shopping_list.append('milk')
 
     final_list = []
     
-    for index, item in enumerate(cleaned, start=1):
+    for index, item in enumerate(shopping_list, start=1):
         formatted_item = item.strip().capitalize()
         final_list.append(f"{index}/ {formatted_item}")
 
