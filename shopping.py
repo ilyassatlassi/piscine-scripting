fa20053fc23ca0f1f2c6c74fcceff09cefed143a
ilyassatlassi <atlassiilyass@gmai.com>
2026-04-07 12:48:33 +0100

    shopping

---
+++ shopping.py
@@ -0,0 +1,5 @@
+def remember_the_apple(shopping_list):
+    if not "apple" in shopping_list:
+        shopping_list.append("apple")
+    return shopping_list    
+        
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-import punishment
+import shopping
 
-print(punishment.do_punishment('   The first half   ', '   and the second  ', 4), end='')
-print(punishment.do_punishment('Will not', 'show', 0), end='')
-print(punishment.do_punishment('', '', 3), end='')
+list_with_apple = ['tomatoes', 'pastas', 'apple', 'salt']
+list_without_apple = ['tomatoes', 'pastas', 'salt']
+
+print(shopping.remember_the_apple(list_with_apple))
+print(shopping.remember_the_apple(list_without_apple))
