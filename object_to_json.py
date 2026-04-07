4dd3e99035cea53d7a81d98b31da462013d4bf3f
ilyassatlassi <atlassiilyass@gmai.com>
2026-04-07 16:14:11 +0100

    object to json

---
+++ object_to_json.py
@@ -0,0 +1,26 @@
+import json
+
+class User:
+    username = 'user'
+    email = 'something@mail.com'
+
+def create_new_user(json_str):
+
+    try:
+        data = json.loads(json_str)
+    except json.JSONDecodeError:
+        return User()
+
+    if 'username' in data and 'email' in data:
+        new_user = User()
+        new_user.username = data['username']
+        new_user.email = data['email']
+        return new_user
+    return User()
+
+def user_to_json(user_obj):
+    if user_obj.username == 'user' and user_obj.email == 'something@mail.com':
+        return json.dumps({})
+    
+    user_data = user_obj.__dict__ 
+    return json.dumps(user_data)
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-import custom_calendar
+from object_to_json import create_new_user, user_to_json
 
-print(custom_calendar.day_from_number(2))
-print(custom_calendar.day_from_number(1))
-print(custom_calendar.day_from_number(1000))
-print(custom_calendar.day_to_number('Sunday'))
-print(custom_calendar.day_to_number('invalid day'))
+registration_0 = '{"username": "mario", "email": "mario@me.it"}'
+registration_1 = '{"city": "Rome", "country": "Italy"}'
+
+user_0 = create_new_user(registration_0)
+user_1 = create_new_user(registration_1)
+
+print(user_to_json(user_0))
+print(user_to_json(user_1))
