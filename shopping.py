54c2c5761fb4b35247a61c1516275c83c3338439
ilyassatlassi <atlassiilyass@gmai.com>
2026-04-07 12:51:12 +0100

    shopping

--- shopping.py
+++ shopping.py
@@ -1,4 +1,6 @@
 def remember_the_apple(shopping_list):
+    if not shopping_list:
+        return shopping_list
     if not "apple" in shopping_list:
         shopping_list.append("apple")
     return shopping_list    
